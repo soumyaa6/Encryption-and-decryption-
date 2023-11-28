1e58cd0fffb08635b1fc6a2ab45e742ad0b08b81
Soumya Hagargi <113289478+soumyaa6@users.noreply.github.com>
2023-11-28 21:45:41 +0530

    Create decryption module.cpp

---
+++ decryption module.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void decryptFile(const string& fileName, int key) {
+    ifstream inputFile(fileName, ios::in);
+
+    if (!inputFile.is_open()) {
+        cerr << "Error opening file: " << fileName << endl;
+        return;
+    }
+
+    string content;
+    char ch;
+    while (inputFile.get(ch)) {
+        content += ch;
+    }
